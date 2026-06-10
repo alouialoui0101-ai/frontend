c2f1a9e1a59c62a5748c4143b2726de9006fa574
“alouialoui0101-ai” <alouialoui0101@gmail.com>
2026-06-10 12:49:14 +0200

    adding file v2

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    // Proxy /api during dev so you can also call relative URLs if you prefer
-    proxy: {
-      '/api': 'http://localhost:5000',
+    plugins: [react()],
+    server: {
+        port: 5173,
+        // Proxy /api during dev so you can also call relative URLs if you prefer
+        proxy: {
+            '/api': 'https://backend-k0en.onrender.com',
+        },
     },
-  },
 });
